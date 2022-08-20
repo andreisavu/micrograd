a012a01bb26b1ddf227994f470eb1055728c9bf9
Andrei Savu <andrei@moonsense.io>
2022-08-20 12:24:15 -0700

    Making the code easier to read and adding more operators

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
+use std::iter::zip;
 use std::ops;
 use std::{cell::RefCell, rc::Rc};
-use std::iter::zip;
 
-#[derive(Debug)]
+// Implementation based on the following tutorial by Andrej Karpathy going into
+// the details on how he built the micrograd library in Python. Doing the same
+// in Rust is not entirely trivial given how the compiler handles state.
+
+// See the video at: https://www.youtube.com/watch?v=VMj-3S1tku0
+// https://github.com/karpathy/micrograd
+
+//
+// Step 1. Define the core state object and the mutable expression graph.
+//
+
+#[derive(Clone, Debug)]
 enum Op {
     None,
     Plus,
     Multiply,
     Tanh,
+    Exp,
+    Pow
 }
 
 #[derive(Debug)]
@@ -31,12 +44,69 @@ impl Value {
         })))
     }
 
+    fn child(&self, index: usize) -> Value {
+        self.0.borrow().children[index].clone()
+    }
+
+    fn only_child(&self) -> Value {
+        assert!(self.0.borrow().children.len() == 1);
+        self.child(0)
+    }
+
+    fn lhs(&self) -> Value {
+        self.child(0)
+    }
+
+    fn rhs(&self) -> Value {
+        self.child(1)
+    }
+
+    fn inc_gradient(&self, amount: f64) {
+        self.0.borrow_mut().gradient += amount
+    }
+
+    fn gradient(&self) -> f64 {
+        self.0.borrow().gradient
+    }
+
+    fn value(&self) -> f64 {
+        self.0.borrow().value
+    }
+
+    fn op(&self) -> Op {
+        self.0.borrow().op.clone()
+    }
+}
+
+//
+// Step 2. Define Standard mathematical operations on Value objects.
+//
+
+impl Value {
     fn tanh(self) -> Value {
         Value(Rc::new(RefCell::new(ValueState {
-            value: self.0.borrow().value.tanh(),
+            value: self.value().tanh(),
             children: vec![self.clone()],
             gradient: 0.0,
             op: Op::Tanh,
+        })))
+    }
+
+    fn exp(self) -> Value {
+        Value(Rc::new(RefCell::new(ValueState {
+            value: self.value().exp(),
+            children: vec![self.clone()],
+            gradient: 0.0,
+            op: Op::Exp,
+        })))
+    }
+
+    fn pow(self, value: f64) -> Value {
+        Value(Rc::new(RefCell::new(ValueState {
+            value: self.value().powf(value),
+            children: vec![self.clone()],
+            gradient: 0.0,
+            op: Op::Pow,
         })))
     }
 }
@@ -46,11 +116,29 @@ impl ops::Add<Value> for Value {
 
     fn add(self, rhs: Value) -> Value {
         Value(Rc::new(RefCell::new(ValueState {
-            value: self.0.borrow().value + rhs.0.borrow().value,
+            value: self.value() + rhs.value(),
             children: vec![self.clone(), rhs.clone()],
             gradient: 0.0,
             op: Op::Plus,
         })))
+    }
+}
+
+impl ops::Add<f64> for Value {
+    type Output = Value;
+
+    fn add(self, rhs: f64) -> Value {
+        self.0.borrow_mut().value += rhs;
+        self
+    }
+}
+
+impl ops::Sub<f64> for Value {
+    type Output = Value;
+
+    fn sub(self, rhs: f64) -> Self::Output {
+        self.0.borrow_mut().value -= rhs;
+        self
     }
 }
 
@@ -59,13 +147,18 @@ impl ops::Mul<Value> for Value {
 
     fn mul(self, rhs: Value) -> Value {
         Value(Rc::new(RefCell::new(ValueState {
-            value: self.0.borrow().value * rhs.0.borrow().value,
+            value: self.value() * rhs.value(),
             children: vec![self.clone(), rhs.clone()],
             gradient: 0.0,
             op: Op::Multiply,
         })))
     }
 }
+
+//
+// Step 3. Each Value object can know its local gradient with respect to
+// the overall expression. This function calculates that gradient value.
+//
 
 fn compute_gradients(out: &Value) {
     out.0.borrow_mut().gradient = 1.0;
@@ -78,22 +171,24 @@ fn compute_gradients(out: &Value) {
     }
 
     fn _compute_children_gradients(node: &Value) {
-        let out_gradient = node.0.borrow().gradient;
-        let out_value = node.0.borrow().value;
-        match node.0.borrow().op {
+        let out_gradient = node.gradient();
+        let out_value = node.value();
+
+        match node.op() {
             Op::Plus => {
-                node.0.borrow().children[0].0.borrow_mut().gradient += out_gradient;
-                node.0.borrow().children[1].0.borrow_mut().gradient += out_gradient;
+                node.lhs().inc_gradient(out_gradient);
+                node.rhs().inc_gradient(out_gradient);
             }
             Op::Multiply => {
-                node.0.borrow().children[0].0.borrow_mut().gradient +=
-                    node.0.borrow().children[1].0.borrow().value * out_gradient;
-                node.0.borrow().children[1].0.borrow_mut().gradient +=
-                    node.0.borrow().children[0].0.borrow_mut().value * out_gradient;
+                node.lhs().inc_gradient(node.rhs().value() * out_gradient);
+                node.rhs().inc_gradient(node.lhs().value() * out_gradient);
             }
             Op::Tanh => {
-                node.0.borrow().children[0].0.borrow_mut().gradient +=
-                    (1.0 - out_value * out_value) * out_gradient;
+                node.only_child()
+                    .inc_gradient((1.0 - out_value * out_value) * out_gradient);
+            }
+            Op::Exp => {
+                node.only_child().inc_gradient(out_value * out_gradient);
             }
             _ => (),
         }
@@ -106,25 +201,12 @@ fn compute_gradients(out: &Value) {
     _compute_children_gradients(out)
 }
 
-fn dump(out: &Value) {
-    fn _dump(out: &Value, prefix: String) {
-        println!(
-            "{}V={:?} O={:?} G={:?} ID={:?}",
-            prefix,
-            out.0.borrow().value,
-            out.0.borrow().op,
-            out.0.borrow().gradient,
-            out.0.as_ptr()
-        );
-        for children in out.0.borrow().children.iter() {
-            _dump(children, format!("{}   ", prefix))
-        }
-    }
-    _dump(out, format!(""));
-}
+//
+// Step 4. Assemble Values into a Neuron structure that use the tanh activation function.
+//
 
 struct Neuron {
-    output: Value
+    output: Value,
 }
 
 impl Neuron {
@@ -145,21 +227,34 @@ impl Neuron {
 
         let bias_value = Value::new(bias);
         Neuron {
-            output: (output + bias_value).tanh()
+            output: (output + bias_value).tanh(),
         }
     }
 }
 
 fn main() {
-    let neuron = Neuron::new(
-        &[2.0, 0.0],
-        &[-3.0, 1.0],
-        6.8813735870195432
-    );
+    let neuron = Neuron::new(&[2.0, 0.0], &[-3.0, 1.0], 6.8813735870195432);
 
     compute_gradients(&neuron.output);
 
     dump(&neuron.output);
+}
+
+fn dump(out: &Value) {
+    fn _dump(out: &Value, prefix: String) {
+        println!(
+            "{}V={:?} O={:?} G={:?} ID={:?}",
+            prefix,
+            out.0.borrow().value,
+            out.0.borrow().op,
+            out.0.borrow().gradient,
+            out.0.as_ptr()
+        );
+        for children in out.0.borrow().children.iter() {
+            _dump(children, format!("{}   ", prefix))
+        }
+    }
+    _dump(out, format!(""));
 }
 
 // fn main() {
